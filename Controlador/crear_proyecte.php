953edc6a0b7dccd03f13aaedcb1459886c41584a
BenitoMF <b.martinez2@sapalomera.cat>
2024-04-30 01:01:02 +0300

    implementar els permissos d'usuaris

--- Controlador/crear_proyecte.php
+++ Controlador/crear_proyecte.php
@@ -4,32 +4,56 @@ include '../Model/mainfunction.php';
 
 $conn = connexio();
 
-$nombre_proyecto = $_POST['nombre_proyecto'];
+
+
+if (isset($_POST['nombre_proyecto'])) {
+
+    $nombre_proyecto = $_POST['nombre_proyecto'];
 $descripcion = $_POST['descripcion'];
 $data_inici = date("Y-m-d");
 $data_fi = $_POST['data_fi'];
 $email_usuario = 'b.martinez2@sapalomera.cat'; 
 //$id_usuari = idUsuariPerEmail($email_usuario);
 $id_usuari = 2;
-
-if (isset($nombre_proyecto)) {
-    $statement = $conn->prepare("INSERT INTO projectes (nom, descripcio, data_inici, data_fi,id_usuari) VALUES (?,?,?,?,?)");
+    $statement = $conn->prepare("INSERT INTO projectes (nom, descripcio, data_inici, data_fi) VALUES (?,?,?,?)");
     $statement->bindParam(1,$nombre_proyecto);
     $statement->bindParam(2,$descripcion);
     $statement->bindParam(3,$data_inici);
     $statement->bindParam(4,$data_fi);
-    $statement->bindParam(5,$id_usuari);
     $statement->execute();
     
 }
 
-$nombre_proyecto_compartido = 8;
-$email_compartido = 3;
 
-if (isset($email_compartido)) {
-    $statement = $conn->prepare("INSERT INTO proyecto_usuario (id_proyecto, id_usuario) VALUES (?,?)");
+
+
+
+if (isset($_POST['email_compartido'])) {
+
+    $nombre_proyecto_compartido = $_POST['nombre_proyecto_compartido'];
+
+    $id_projecte = "";
+    $statement = $conn->prepare("SELECT id FROM projectes WHERE nom = ?");
     $statement->bindParam(1,$nombre_proyecto_compartido);
+    $statement->execute();
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $id_projecte = $row["id"];
+    }
+echo $id_projecte;
+    $email_compartido = $_POST['email_compartido'];
+    $id_usuari = "";
+    $statement = $conn->prepare("SELECT id FROM usuaris WHERE email = ?");
+    $statement->bindParam(1,$email_compartido);
+    $statement->execute();
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $id_usuari = $row["id"];
+    }
+    echo $id_usuari;
+    $permissos = 'editar';
+    $statement = $conn->prepare("INSERT INTO proyecto_usuario (id_proyecto, id_usuario, permissos) VALUES (?,?,?)");
+    $statement->bindParam(1,$id_projecte);
     $statement->bindParam(2,$id_usuari);
+    $statement->bindParam(3,$permissos);
     $statement->execute();
 }
 
--- Vista/creacio_projecte.vista.php
+++ Vista/creacio_projecte.vista.php
@@ -4,99 +4,6 @@
     <title>Crear Proyecto</title>
     <style>
 /* Style for the body */
-body {
-    font-family: Arial, sans-serif;
-    background-color: #f2f2f2;
-    margin: 0;
-    padding: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-}
-
-.container {
-    max-width: 500px;
-    width: 100%;
-}
-
-form {
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
-    margin-bottom: 20px;
-}
-
-h2 {
-    color: #333;
-    margin-bottom: 20px; /* Add space between title and form */
-}
-
-label {
-    font-weight: bold;
-}
-input[type="text"],
-input[type="date"],
-textarea {
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-}
-
-
-input[type="submit"] {
-    background-color: #007bff;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-}
-
-input[type="submit"]:hover {
-    background-color: #0056b3;
-}
-
-input:invalid {
-    border-color: #dc3545;
-}
-
-@media only screen and (max-width: 600px) {
-    body {
-        padding: 10px;
-    }
-}
-
-.container {
-    max-width: 800px; 
-    margin: 0 auto; 
-}
-
-
-.forms-container {
-    display: flex; 
-}
-
-
-form {
-    flex: 1;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
-    margin: 0 10px; /* Espacio entre los formularios */
-}
-
-/* Estilo para el título */
-h2 {
-    text-align: center; /* Centra el título horizontalmente */
-    margin-bottom: 20px; /* Espacio entre el título y los formularios */
-}
-
     </style></head>
 <body>
 <div class="container">
@@ -116,7 +23,7 @@ h2 {
     </form>
     <form id="second-form" action="../Controlador/crear_proyecte.php" method="post">
         <label for="nombre_proyecto_compartido">Nombre del Proyecto:</label><br>
-        <input type="text" id="nombre_proyecto_compartido" name="nombre_proyecto_compartido" required value="DVD2"><br><br>
+        <input type="text" id="nombre_proyecto_compartido" name="nombre_proyecto_compartido" required><br><br>
         
         <label for="email_compartido">email:</label><br>
         <input type="text" id="email_compartido" name="email_compartido" required><br><br>
